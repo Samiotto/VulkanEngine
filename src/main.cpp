bbba50dddda47cfb920c225ccdfae708cd40b35b
Samiotto <samjgarcia99@gmail.com>
2025-11-20 17:59:36 -0600

    Link Vulkan SDK and create Vulkan instance
    Cmake not sees the vulkan sdk and we now create a vulkan instance with necessary extensions which can be used during the rendering loop.

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,9 @@
 #include <SDL.h>
+#include <SDL_vulkan.h>
+#include <vulkan/vulkan.h>
+
 #include <iostream>
+#include <vector>
 
 
 int main(int argc, char *argv[]) {
@@ -24,6 +28,55 @@ int main(int argc, char *argv[]) {
         return 2;
     }
 
+     // Query required vulkan instance extensions from SDL
+        unsigned int sdlExtensionsCount = 0;
+        if (!SDL_Vulkan_GetInstanceExtensions(window, &sdlExtensionsCount, nullptr)) {
+            std::cerr << "Failed to get SDL Vulkan extensions: " << SDL_GetError() << std::endl;
+            SDL_DestroyWindow(window);
+            SDL_Quit();
+            return 3;
+        }
+
+        std::vector<const char*> extensions(sdlExtensionsCount);
+        if (!SDL_Vulkan_GetInstanceExtensions(window, &sdlExtensionsCount, extensions.data())) {
+            std::cerr << "Failed to get SDL Vulkan extensions (2nd Call): " << SDL_GetError() << std::endl;
+            SDL_DestroyWindow(window);
+            SDL_Quit();
+            return 4;
+        }
+
+        // Fill out Vulkan application info
+        // vulkan instance
+        VkApplicationInfo appInfo{};
+        appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+        appInfo.pApplicationName = "Vulkan Engine Application";
+        appInfo.applicationVersion = VK_MAKE_VERSION(0, 1, 0);
+        appInfo.pEngineName = "Vulkan Engine Engine";
+        appInfo.engineVersion = VK_MAKE_VERSION(0, 1, 0);
+        appInfo.apiVersion = VK_API_VERSION_1_2;
+
+        // instance create info
+        VkInstanceCreateInfo createInfo{};
+        createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+        createInfo.pApplicationInfo = &appInfo;
+
+        createInfo.enabledExtensionCount = static_cast<uint32_t>(extensions.size());
+        createInfo.ppEnabledExtensionNames = extensions.data();
+
+        // TODO: currently no validation layers
+        createInfo.enabledLayerCount = 0;
+        createInfo.ppEnabledLayerNames = nullptr;
+
+        // create the instance
+        VkInstance instance = VK_NULL_HANDLE;
+        VkResult result = vkCreateInstance(&createInfo, nullptr, &instance);
+        if (result != VK_SUCCESS) {
+            std::cerr << "vkCreateInstance failed with error code " << result << std::endl;
+            SDL_DestroyWindow(window);
+            SDL_Quit();
+            return 5;
+        }
+
     bool running = true;
     while (running) {
         SDL_Event event;
@@ -38,9 +91,11 @@ int main(int argc, char *argv[]) {
 
         // do rendering stuff and things
 
+
         SDL_Delay(10);
     }
 
+    vkDestroyInstance(instance, nullptr);
     SDL_DestroyWindow(window);
     SDL_Quit();
     return 0;
